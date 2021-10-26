a6f4c522524d3fe8c95a96494c3e54b03ab15e21
Hoang Kiem <4584599+binjuhor@users.noreply.github.com>
2021-10-26 17:57:12 +0700

    Add link text to index.html Add time to coundown folder

--- coundown-svg-icon/js/script.js
+++ coundown-svg-icon/js/script.js
@@ -1,25 +1,31 @@
 (function ($) {
     "use strict";
-    let day = '2021/01/01 00:00:00';
+    const curentDate = new Date()
+    const addHoursToDate = (date, hours) => {
+        return new Date(new Date(date).setHours(date.getHours() + hours))
+    }
+    
+    const day = addHoursToDate(curentDate, 320)
+
 
     $('#days').countdown(day, function(event) {
         $(this).html(event.strftime('%D'));
-        roundSVGStyle('.round-days', (event.strftime('%D')/365)*100);
+        roundSVGStyle('.round-days', (event.strftime('%D')/365)*100)
     });
 
     $('#hours').countdown(day, function(event) {
         $(this).html(event.strftime('%H'));
-        roundSVGStyle('.round-hours', (event.strftime('%H')/24)*100);
+        roundSVGStyle('.round-hours', (event.strftime('%H')/24)*100)
     });
 
     $('#minutes').countdown(day, function(event) {
         $(this).html(event.strftime('%M'));
-        roundSVGStyle('.round-minutes', (event.strftime('%M')/60)*100);
+        roundSVGStyle('.round-minutes', (event.strftime('%M')/60)*100)
     });
 
     $('#second').countdown(day, function(event) {
         $(this).html(event.strftime('%S'));
-        roundSVGStyle('.round-second', (event.strftime('%S')/60)*100);
+        roundSVGStyle('.round-second', (event.strftime('%S')/60)*100)
     });
 
     function roundSVGStyle(className='', percent=0 )
@@ -30,4 +36,4 @@
             roundDraw = roundPercent * roundCircum / 100;
         $round.attr('style', `stroke-dasharray: ${roundDraw} 999`);
     }
-})(jQuery);
+})(jQuery)
